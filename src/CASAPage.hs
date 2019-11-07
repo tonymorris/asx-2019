2bbb202b576e862f2bb0831c72a8aecf6f73bde9
Tony Morris <tmorris@tmorris.net>
2019-11-07 11:19:57 +1000

    this is way fun

--- src/CASAPage.hs
+++ src/CASAPage.hs
@@ -2,12 +2,20 @@
 
 module CASAPage where
 
+import Acronym
+import Acronyms
+import Control.Applicative
 import Control.Lens
 import Control.Monad
-import Data.ByteString.Lazy
+import Data.Bool
+import Data.Char
+import Data.ByteString.Lazy(ByteString, unpack)
+import Data.ByteString.Lazy.Char8 as Char8(unpack)
 import Network.Wreq
 import Text.HTML.TagSoup.Navigate
 import Control.Monad.Trans.Maybe
+import Control.Monad.Reader.Class
+import Data.List.Split
 
 casaPage :: MaybeT IO (TagTreePos ByteString)
 casaPage =
@@ -32,21 +40,62 @@ tbody =
           firstChild
           findContentUntil nextSibling (isTagBranch "tr")
 
-breadthFirst p = findContentUntil undefined
-depthFirst p = findContentUntil undefined
+breadthFirst p s = 
+  reader (p . view tagTreePosContent) >>= bool undefined (return s)
+
+depthFirst p = findContentUntil undefined p
+
+trim =
+  let k = dropWhile isSpace . reverse
+  in  k . k . Char8.unpack
+
+{-
+data Source =
+  ICAO
+  | AIP
+  | Other String
+  deriving (Eq, Ord, Show)
+
+parseSource "ICAO" = ICAO
+parseSource "AIP" = AIP
+parseSource s = Other s
+-}
+
+parseSources s =
+  splitOn "/" s
 
 parsing =
   do  tbody
-      firstChild
+      a <- parseAcronym
+      b <- parseAcronym
+      return (a, b)
+
+parseAcronyms =
+  Acronyms <$> many parseAcronym  
+      
+parseAcronym =
+  do  firstChild
       findContentUntil nextSibling (isTagBranch "td")
       firstChild
-      c <- content
-      c' <- maybeTagTreePosState (c ^? _TagLeaf . _TagText)
-      pure c'
+      n <- opticContent (_TagLeaf . _TagText)
+      parent
+      nextSibling
+      nextSibling
+      firstChild
+      m <- opticContent (_TagLeaf . _TagText)
+      parent
+      nextSibling
+      nextSibling
+      firstChild
+      s <- opticContent (_TagLeaf . _TagText)
+      parent
+      parent
+      nextSibling
+      nextSibling
+      pure (Acronym (trim n) (trim m) (parseSources (trim s)))
 
-      -- z <- casaPage
-
-      
+debug2 =
+  fmap (over _1 (view tagTreePosContent)) . runTagTreePosState (breadthFirst (isTagBranch "tbody") undefined) <$> casaPage
 
 debug =
   fmap (over _1 (view tagTreePosContent)) . runTagTreePosState parsing <$> casaPage
